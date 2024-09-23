8232935bd22bcbcec6970dfded366db5d5593339
rox-dotcom <roxarandagr@gmail.com>
2024-09-22 22:21:22 -0600

    Se termino el algoritmo

--- encriptado.cpp
+++ encriptado.cpp
@@ -2,25 +2,28 @@
 #include "confusion.h"
 #include "difusion.h"
 
-//Función para mezclar la llave con el texto
-string mezclardesmezclarConLlave(const string &texto, const string &llave) {
+// Función para mezclar la llave con el texto
+string mezclardesmezclarConLlave(const string &texto, const string &llave)
+{
     string resultado = texto;
-    for (size_t i = 0; i < texto.length(); i++) {
+    for (size_t i = 0; i < texto.length(); i++)
+    {
         resultado[i] = texto[i] ^ llave[i % llave.length()]; // XOR entre el texto y la llave
     }
     return resultado;
 }
 
-//Función para encriptar texto con un desplazamiento y una lista de índices de permutación y una llave
-string encriptar(const string &texto, const string &llave, int desplazamiento, const vector<int> &indices) {
+// Función para encriptar texto con un desplazamiento y una lista de índices de permutación y una llave
+string encriptar(const string &texto, const string &llave, int desplazamiento, const vector<int> &indices)
+{
     // Mezclar el texto con la llave usando XOR
     string textoMezclado = mezclardesmezclarConLlave(texto, llave);
 
     // Aplicar la sustitución (confusión)
     string textoConfuso = sustitucion(textoMezclado, desplazamiento);
-    
+
     // Aplicar la permutación (difusión)
     string textoPermutado = permutar(textoConfuso, indices);
-    
+
     return textoPermutado;
 }
